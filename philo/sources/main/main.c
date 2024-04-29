fb9776a9858539524f7351ad37fb8e1af23fc47f
Florian Berthouly <fberthou@z1r1p5.42lyon.fr>
2024-04-29 10:59:56 +0200

    built

--- philo/sources/main/main.c
+++ philo/sources/main/main.c
@@ -6,42 +6,29 @@
 /*   By: fberthou <fberthou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 11:06:29 by fberthou          #+#    #+#             */
-/*   Updated: 2024/04/29 08:47:04 by fberthou         ###   ########.fr       */
+/*   Updated: 2024/04/29 10:30:13 by fberthou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
 #include <pthread.h>
 #include <sys/time.h>
 
+#include <unistd.h>
+
 /* ====	PROTOTYPES	==== */
-int	parsing(int argc, char **argv, int *tab_arg);
+int		parsing(int argc, char **argv, int *tab_arg);
+int		print_error(char *str);
+void	*ft_calloc(size_t nmemb, size_t size);
 /* ====	PROTOTYPES	==== */
-
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i++])
-		;
-	return (i);
-}
-
-int	print_error(char *str)
-{
-	return (write(2, str, ft_strlen(str)));
-}
 
 void	*sleep_and_die(void *tab_arg)
 {
 	static pthread_t	new;
 	struct timeval		tv[3];
-	int tmp;
+	int					tmp;
+
 	if (!new)
 	{
 		if (pthread_create(&new, NULL, sleep_and_die, tab_arg))
@@ -63,10 +50,71 @@ void	*sleep_and_die(void *tab_arg)
 	return (NULL);
 }
 
+typedef struct s_philo
+{
+	pthread_t		philo_id;
+	pthread_t		*philo_head;
+	int				*args;
+	struct s_philo	*next;
+}	t_philo ;
+
+int	join_thread(t_philo *lst)
+{
+	t_philo	*head;
+
+	head = lst;
+	while (lst)
+	{
+		if (pthread_join(lst->philo_id, NULL))
+			return (1);
+		lst = lst->next;
+	}
+}
+
+
+void	*start_routine(void *data)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *) data;
+	printf("%ld arrive\n", philo->philo_id);
+	return (NULL);
+}
+
+t_philo	*socrate_maker(int *tab_args)
+{
+	t_philo	*lst;
+	t_philo	*head;
+	int		tmp;
+
+	tmp = tab_args[0];
+	lst = ft_calloc(1, sizeof(t_philo));
+	if (!lst)
+		return (NULL);
+	lst->args = tab_args;
+	if (pthread_create(&lst->philo_id, NULL, start_routine, tab_args))
+		return (NULL);
+	lst->philo_head = &lst->philo_id;
+	lst->next = NULL;
+	head = lst;
+	while (--tmp)
+	{
+		lst->next = ft_calloc(1, sizeof(t_philo));
+		if (!lst->next)
+			return (/*free head*/NULL);
+		if (pthread_create(&lst->next->philo_id, NULL, start_routine, tab_args))
+			return (/*free lst*/NULL);
+		lst->next->philo_head = &head->philo_id;
+		lst = lst->next;
+	}
+	return (lst);
+}
+
 int	main(int argc, char **argv)
 {
-	int	tab_arg[5];
-	pthread_t	*th;
+	int			tab_arg[5];
+	t_philo		*head_lst;
+	pthread_t	**th;
 
 	if (argc != 5 && argc != 6)
 		return (print_error("Nb of arguments is invalid\n"), 0);
@@ -74,17 +122,30 @@ int	main(int argc, char **argv)
 		return (0);
 	if (tab_arg[0] == 1)
 	{
-		th = sleep_and_die(tab_arg);
-		if (th)
-			pthread_join(*th, NULL);
-    // else
-      //print error
+		*th = (pthread_t *) sleep_and_die(tab_arg);
+		if (*th)
+			pthread_join(**th, NULL);
+    	else
+    		return (print_error("Crash thread initialisation\n"), 0);
+	}
+
+
+
+	else if (argc == 5)
+	{
+		*th = socrate_maker(tab_arg[0]);
+		if (!*th)
+    	  return (print_error("Crash thread initialisation\n"), 0);
+		if (join_thread(*th))
+			return (print_error("Error -> join\n"), 0);
+		while(tab_arg[0]--)
+		{
+			free(*th);
+		}
 	}
 	/*
 	if (tab_arg[1] < tab_arg[2] || tab_arg[1] < tab_arg[3])
 		// incoherent argument
-	if (argc == 5)
-		// eternals philo
 	else
 		// eat %d time and die
 	*/
---
+++ philo/sources/main/utils.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fberthou <fberthou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 08:51:26 by fberthou          #+#    #+#             */
+/*   Updated: 2024/04/29 09:31:22 by fberthou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i++])
+		;
+	return (i);
+}
+
+int	print_error(char *str)
+{
+	return (write(2, str, ft_strlen(str)));
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*new_mem_place;
+	size_t	total_memory_size;
+
+	total_memory_size = nmemb * size;
+	if (total_memory_size == 0 || (total_memory_size / nmemb != size))
+		return (NULL);
+	new_mem_place = malloc(total_memory_size);
+	if (!new_mem_place)
+		return (NULL);
+	memset(new_mem_place, 0, total_memory_size);
+	return (new_mem_place);
+}
